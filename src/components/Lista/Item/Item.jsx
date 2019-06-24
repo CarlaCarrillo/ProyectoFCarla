90b583c1764c4f319f35365795dce315f8f4dc7a
CarlaCarrillo <ccarrillo1616@gmail.com>
2019-06-24 16:23:27 -0500

    Se adiciona botón de agregar cantidad de producto

--- src/components/Lista/Item/Item.jsx
+++ src/components/Lista/Item/Item.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 //import UpdateItem from '../UpdateItem/UpdateItem';
 import { deleteData } from '../../utils/api'
 import Button from 'react-bootstrap/Button';
+
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -27,7 +28,7 @@ class Item extends Component {
 
   increaseClick() {
     this.setState({clicked: this.state.clicked + 1});
-    this.props.changeLastClicked(this.props.id);
+    
   }
 
   editItem() {
@@ -46,32 +47,63 @@ class Item extends Component {
   render() {
     const {id, name, description, costo,imagen} = this.props;
     return (
-       
-       <Row id='cartas'>
-        <Col sm={4} md={4}>
-      <Container style={{ marginTop: '2rem', marginBottom: '2rem' }}>     
-      <Card id="carta" style={{ width: '14rem' }}>
-      <Card.Img variant="top" src={imagen} alt="242x200" />
-      <Card.Body>
-          <Card.Title><h5>{name}</h5></Card.Title>
-          <Card.Text>
-            <h5>{description}</h5>
-            <h2>$ {costo}</h2>  
-            <tr onClick={this.increaseClick}>
-              <td>{this.props.id === this.props.lastClicked && 'Last Clicked'}</td>
-              </tr>      
-          </Card.Text>
-          <Button variant="outline-danger" onClick={this.removeItem}>Remove</Button>
-      </Card.Body>
       
-      </Card>
-      </Container>
-      </Col> 
-      </Row>
-      
+      <Row id='cartas'>
+<Col sm={4} md={4}>
+<Container style={{ marginTop: '2rem', marginBottom: '2rem' }}>     
+<Card id="carta" style={{ width: '14rem' }}>
+<Card.Img variant="top" src={imagen} alt="242x200" />
+<Card.Body>
+  <Card.Title><h5>{name}</h5></Card.Title>
+  <Card.Text>
+    <h5>{description}</h5>
+    <h2>$ {costo}</h2>  
+  </Card.Text>
+  
+  <tr onClick={this.increaseClick}>
+  <td><Button  variant="light" onClick={this.increaseClick}>Cantidad</Button></td>
+  <td>{this.state.clicked}</td>
+  </tr><p/>
+  
+  <Button variant="outline-dark" onClick={this.removeItem}>Remove</Button>
+</Card.Body>
+
+</Card>
+</Container>
+</Col> 
+</Row>
+
+
     );
   }
 }
-
 export default Item;
 
+//<Button variant="outline-danger" onClick={this.removeItem}>Remove</Button>
+/*
+<Row id='cartas'>
+<Col sm={4} md={4}>
+<Container style={{ marginTop: '2rem', marginBottom: '2rem' }}>     
+<Card id="carta" style={{ width: '14rem' }}>
+<Card.Img variant="top" src={imagen} alt="242x200" />
+<Card.Body>
+  <Card.Title><h5>{name}</h5></Card.Title>
+  <Card.Text>
+    <h5>{description}</h5>
+    <h2>$ {costo}</h2>  
+    <tr onClick={this.increaseClick}>
+      <td>Catidad {this.props.id === this.props.lastClicked && 'Last Clicked'}</td>
+    </tr>      
+  </Card.Text>
+  <tr onClick={this.increaseClick}>
+  <td>Cantidad = {this.state.clicked}</td>
+  <Button variant="outline-danger" onClick={this.increaseClick}>Cantidad</Button>
+  </tr>
+  <Button variant="outline-danger" onClick={this.removeItem}>Remove</Button>
+</Card.Body>
+
+</Card>
+</Container>
+</Col> 
+</Row>
+*/
